7a8d7a9da792495e2ea0ed5c12e2df343df199a6
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2020-02-04 14:00:27 +0100

    Set environment variables

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ async function run() {
   try {
     const pluginName = core.getInput('plugin-name');
 
+    await core.exportVariable('RAILS_ENV', 'test');
+    await core.exportVariable('DATABASE_URL', 'postgresql://postgres:@localhost/test');
+    await core.exportVariable('DATABASE_CLEANER_ALLOW_REMOTE_DATABASE_URL', 'true');
+
     await exec.exec('sudo apt-get install build-essential libcurl4-openssl-dev libvirt-dev ruby-libvirt zlib1g-dev libpq-dev');
     await exec.exec('gem install bundler');
     await exec.exec(`echo "gemspec :path => '${pluginName}'" > bundler.d/${pluginName}.local.rb`);
