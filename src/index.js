00ae09c625f83095deedd065907c0fee6b02f1dd
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2020-03-06 09:50:08 +0100

    Make it possible to specify custom Foreman path

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ async function run() {
   try {
     const pluginName = core.getInput('plugin-name');
     const pluginPath = core.getInput('plugin-path') || `./${pluginName}`;
+    const foremanPath = core.getInput('foreman-path') || './';
 
     await core.exportVariable('RAILS_ENV', 'test');
     await core.exportVariable('DATABASE_URL', 'postgresql://postgres:@localhost/test');
     await core.exportVariable('DATABASE_CLEANER_ALLOW_REMOTE_DATABASE_URL', 'true');
+
+    // await exec.exec(`cd ${foremanPath}`);
+    const { execSync } = require('child_process');
+    let stdout = execSync(`cd ${foremanPath}`);
 
     await exec.exec('sudo apt-get install build-essential libcurl4-openssl-dev libvirt-dev ruby-libvirt zlib1g-dev libpq-dev');
     await exec.exec('gem install bundler');
